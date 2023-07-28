a6117e9d1520278a5e5804087676e6415ac2d625
Art Frich <frich.g23@yandex.ru>
2023-07-28 05:35:26 +0500

    fix: git autotests

--- castomErrors/NotFoundError.js
+++ castomErrors/NotFoundError.js
@@ -1,8 +1,8 @@
-const { NOT_FOUND_STATUS, NOT_FOUND_MSG } = require('../helpers/constants');
+const { NOT_FOUND_STATUS, NOT_FOUND_TEXT = 'Не удалось найти искомый объект.' } = require('../helpers/constants');
 
 class NotFoundError extends Error {
   constructor() {
-    super(NOT_FOUND_MSG);
+    super(NOT_FOUND_TEXT);
     this.status = NOT_FOUND_STATUS;
   }
 }
